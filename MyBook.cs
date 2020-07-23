24f64aacf3cd8b86a3d0ea5bd01226109a946f6e
AaqibGitBit <ahamedaaqib96@gmail.com>
2020-07-23 09:26:08 +0530

    MyBooK

---
+++ MyBook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code_Challenge_Day_13_Abstract_Classes
+{
+     class MyBook : Book
+    {
+       
+         int price;
+
+        public MyBook(string title, string author, int price) : base(title,author)
+        {
+            this.price = price;
+        }
+        public override void display()
+        {
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Author: " + author);
+            Console.WriteLine("Price: " + price);
+        }
+    }
+}
